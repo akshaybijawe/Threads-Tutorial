313d83549b40e1a57832632a569562d49f950c03
Akshay Bijawe <akshaybijawe121@gmail.com>
2018-03-20 22:47:20 -0700

    Add the code without making synchronization

---
+++ src/com/akshaybijawe/synchronization/ThreadsSyncExample.java
@@ -0,0 +1,33 @@
+package com.akshaybijawe.synchronization;
+
+/**
+ * @author arbijawe on 3/20/18
+ * @project Threads-Tutorial
+ */
+
+public class ThreadsSyncExample {
+
+    //define a class variable called counter
+    static int counter = 1;
+
+    public static void main(String[] args) {
+
+        //use lambda notation for the runnable method
+        Runnable r = () ->
+        {
+            System.out.println("ID value: " + getID());
+        };
+
+        Thread one = new Thread(r,"one");
+        one.start();
+
+        Thread two = new Thread(r, "two");
+        two.start();
+
+
+    }
+
+    public static int getID(){
+        return counter++;
+    }
+}
